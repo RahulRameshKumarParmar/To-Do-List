60c10d3bd4bc58d34f9035ac02174f2f6d1860ec
RahulRameshKumarParmar <rahulparmar1339950@gmail.com>
2025-06-17 19:53:46 +0530

    Add files via upload

---
+++ javascript/LocalStorage.js
@@ -0,0 +1,223 @@
+export let taskBtn, settingBtn, saveBtn, addTaskBtn, deleteProjectBtn, submitBtn;
+
+let newTaskModel, cancelAllBtn, overlay, taskList, deleteProjectName, projectSettingBoxContainer, title, startDate, endDate, task, projectTitle, projectName, mdTaskBtn, mdSettingBtn, threeDotBtn, smallScreenProjectTitleBox, titleBox, managerBox, emailBox;
+
+export function HTMLcontentLoad() {
+    taskBtn = document.querySelector(".taskBtn");
+    settingBtn = document.querySelector(".settingBtn");
+    saveBtn = document.querySelector(".saveBtn");
+    addTaskBtn = document.querySelector(".addTaskBtn");
+    submitBtn = document.querySelector(".submitBtn");
+    deleteProjectBtn = document.querySelector(".deleteProjectBtn")
+    projectTitle = document.getElementById("projectTitle");
+    projectName = localStorage.getItem("projectName");
+    taskList = document.getElementById("taskList");
+}
+
+title = document.querySelector(".taskTitle");
+startDate = document.querySelector(".startDate");
+endDate = document.querySelector(".endDate");
+newTaskModel = document.querySelector(".newTaskModel");
+projectSettingBoxContainer = document.querySelector(".projectSettingBoxContainer");
+cancelAllBtn = document.querySelectorAll(".cancelBtn, .closeBtn");
+overlay = document.querySelector(".overlay");
+deleteProjectName = document.querySelector(".projectName");
+mdTaskBtn = document.querySelector(".mdTaskBtn");
+mdSettingBtn = document.querySelector(".mdSettingBtn");
+threeDotBtn = document.querySelector(".threeDotBtn");
+smallScreenProjectTitleBox = document.querySelector(".smallScreenProjectTitleBox");
+titleBox = document.querySelector(".titleBox");
+managerBox = document.querySelector(".managerBox");
+emailBox = document.querySelector(".emailBox");
+
+export function showProjectName() {
+    if (projectTitle) {
+        if (projectName) {
+            projectTitle.textContent = projectName;
+        }
+        else {
+            projectTitle.textContent = "Untitled Project";
+        }
+    }
+}
+
+export function windowLoad() {
+    taskBtn?.classList.add("active");
+    projectSettingBoxContainer?.classList.add("hideDetails");
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    savedTasks.forEach(task => renderTask(task))
+}
+
+export function saveProjectName() {
+    projectName = document.getElementById("projectT").value;
+
+    if (!titleBox.value && !managerBox.value && !emailBox.value) {
+        alert("please fill all details");
+    }
+    else {
+        localStorage.setItem("projectName", projectName);
+        window.location.href = "home.html";
+    }
+}
+
+export function addTaskModel() {
+    if (addTaskBtn && cancelAllBtn && newTaskModel && overlay) {
+
+        addTaskBtn.addEventListener("click", () => {
+            newTaskModel.classList.add("showNewTaskModel");
+            overlay.classList.add("showOverlay");
+        })
+
+        cancelAllBtn.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                newTaskModel.classList.remove("showNewTaskModel");
+                overlay.classList.remove("showOverlay");
+            })
+        })
+    }
+}
+
+export function submitSaveDetails() {
+    if (!title.value || !startDate.value || !endDate.value) {
+        alert("please fill required fields");
+    }
+    else {
+        task = {
+            id: Date.now(),
+            title: title.value,
+            startDate: startDate.value,
+            endDate: endDate.value
+        };
+
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        renderTask(task);
+
+        // Clear Fields
+
+        task.title = "";
+        task.startDate = "";
+        task.endDate = "";
+
+        newTaskModel.classList.remove("showNewTaskModel");
+        overlay.classList.remove("showOverlay");
+    }
+
+}
+
+export function showTask() {
+    taskList.innerHTML = "";
+    taskBtn.classList.add("active");
+    settingBtn.classList.remove('active');
+    projectSettingBoxContainer.classList.add("hideDetails");
+    taskList.classList.remove("hideDetails");
+    addTaskBtn.classList.remove("hideDetails");
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    savedTasks.forEach(task => renderTask(task))
+}
+
+export function renderTask(task) {
+    const checkBtn = document.createElement("input");
+    checkBtn.type = "checkbox";
+    checkBtn.className = "checkBtn";
+
+    const li = document.createElement("li");
+    li.className = "toDoListItems";
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    // const start = new Date(task.startDate);
+    const end = new Date(task.endDate);
+
+    const diffDate = end - today;
+    const days = Math.ceil(diffDate / (1000 * 60 * 60 * 24));
+
+    const daysLeft = document.createElement("span");
+
+    const divDays = document.createElement("div");
+    const divTask = document.createElement("div");
+
+    if (days < 0) {
+        daysLeft.textContent = `Days Late: ${Math.abs(days)}`
+        daysLeft.className = "redText"
+    }
+    else {
+        daysLeft.textContent = `Days Left: ${days}`;
+        daysLeft.className = "greenText"
+    }
+
+    const text = document.createTextNode(`${task.title}`);
+
+
+    divDays.appendChild(daysLeft);
+
+    divTask.appendChild(checkBtn);
+    divTask.appendChild(text);
+
+    li.appendChild(divDays);
+    li.appendChild(divTask);
+
+    taskList?.appendChild(li);
+
+    checkBtn.addEventListener("click", () => {
+        deleteTaskById(task.id, li, days);
+    })
+}
+
+export function showSetting() {
+    taskBtn.classList.remove("active");
+    settingBtn.classList.add('active');
+    projectSettingBoxContainer.classList.remove("hideDetails");
+    taskList.classList.add("hideDetails");
+    addTaskBtn.classList.add("hideDetails");
+}
+
+export function hideSetting() {
+    projectSettingBoxContainer?.classList.add("hideDetails");
+    addTaskBtn?.classList.add("hideDetails");
+}
+
+export function deleteProject() {
+    const projectName = localStorage.getItem("projectName");
+    if (projectName === deleteProjectName.value) {
+        localStorage.removeItem("projectName");
+        localStorage.removeItem("tasks");
+
+        window.location.href = "new_project.html";
+    }
+}
+
+export function deleteTaskById(idToDelete, list, days) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const updatedTask = tasks.filter(task => task.id !== idToDelete);
+
+    localStorage.setItem("tasks", JSON.stringify(updatedTask));
+
+    list.remove();
+
+    if (days < 0) {
+        alert(`Task is completed ${Math.abs(days)} days late`);
+    }
+    else {
+        alert(`Task is completed ${days} days early`);
+    }
+}
+
+threeDotBtn?.addEventListener("click", () => {
+    smallScreenProjectTitleBox.classList.toggle("showOverlay");
+})
+
+mdTaskBtn?.addEventListener("click", () => {
+    mdTaskBtn.classList.add("active");
+    mdSettingBtn.classList.remove("active");
+    showTask();
+})
+
+mdSettingBtn?.addEventListener("click", () => {
+    mdSettingBtn.classList.add("active");
+    mdTaskBtn.classList.remove("active");
+    showSetting();
+})
---
+++ javascript/logic.js
@@ -0,0 +1,130 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+import { taskBtn, showTask, settingBtn, showSetting, saveBtn, submitSaveDetails, addTaskBtn, addTaskModel, deleteProjectBtn, deleteProject, submitBtn, saveProjectName, windowLoad, HTMLcontentLoad, hideSetting, showProjectName} from "./LocalStorage.js";
+
+async function navbar() {
+    const response = await fetch("navbar.html");
+    const data = await response.text();
+    document.getElementById("navbar-container").innerHTML = data;
+
+    const menu_btn = document.querySelector(".menu-btn");
+    const offCanvas = document.querySelector(".off-canvas");
+
+    menu_btn.addEventListener("click", () => {
+        offCanvas.classList.toggle("showCanvas");
+    });
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyA7TEpABRCBYkLChDY2Hr2Np9QrhMIYS7g",
+        authDomain: "todoso-736e4.firebaseapp.com",
+        projectId: "todoso-736e4",
+        storageBucket: "todoso-736e4.appspot.com",
+        messagingSenderId: "454335264141",
+        appId: "1:454335264141:web:25d6b459198b7a5bc739c4",
+        measurementId: "G-L21K8HWX15"
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+    const signInBtn = document.querySelector("#googleBtn");
+    const logIn = document.querySelector(".log-in");
+    const logOut = document.querySelector(".logOut");
+    const signOutBtn = document.querySelectorAll("#signOutBtn, #offSignOutBtn");
+    const offSignOut = document.querySelector(".off-signOut");
+    const offLogin = document.querySelector("#offLoginLink");
+
+    if (signInBtn) {
+        signInBtn.onclick = () => {
+            signInWithPopup(auth, provider)
+                .then(() => window.location.href = "new_project.html")
+                .catch((error) => alert("Error: " + error.message));
+        };
+    }
+
+    if (signOutBtn) {
+        signOutBtn.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                signOut(auth)
+                    .then(() => window.location.href = "log_in.html"
+                    )
+            })
+        })
+    }
+
+    onAuthStateChanged(auth, (user) => {
+        HTMLcontentLoad();
+        if (logIn && logOut) {
+            const formSubmit = document.querySelector(".formSubmit");
+            if (user) {
+                logIn.classList.remove("show-desktop");
+                logOut.classList.add("show-desktop");
+                offLogin.classList.add("hideLogIn");
+                offSignOut.classList.remove("hideLogOut");
+
+                showProjectName();
+                windowLoad();
+
+                formSubmit?.addEventListener("submit", (event) => {
+                    event.preventDefault();
+                })
+
+                submitBtn?.addEventListener("click", () => {
+                    saveProjectName();
+                    localStorage.removeItem("tasks");
+                })
+
+                taskBtn?.addEventListener("click", () => {
+                    showTask();
+                });
+
+                settingBtn?.addEventListener("click", () => {
+                    showSetting();
+                })
+
+                saveBtn?.addEventListener("click", () => {
+                    submitSaveDetails();
+                })
+
+                addTaskBtn?.addEventListener("click", () => {
+                    addTaskModel();
+                })
+
+                deleteProjectBtn?.addEventListener("click", () => {
+                    deleteProject();
+                })
+            }
+            else {
+                logIn.classList.add("show-desktop");
+                logOut.classList.remove("show-desktop");
+                offLogin.classList.remove("hideLogIn");
+                offSignOut.classList.add("hideLogOut");
+
+                hideSetting();
+
+                formSubmit?.addEventListener("submit", (event) => {
+                    event.preventDefault();
+                    window.location.href = "log_in.html";
+                })
+            }
+        }
+    })
+};
+navbar();
+
+async function footer() {
+    const response = await fetch("footer.html");
+    const data = await response.text();
+    const footerContainer = document.getElementById("footer-container");
+
+    if (footerContainer) {
+        footerContainer.innerHTML = data;
+    }
+    else {
+        console.warn("No #footer-container found, skipping footer injection.");
+    }
+};
+
+footer();
